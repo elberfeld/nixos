f7b538528ffdc8f6c5665addb1345eacd61b0049
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2023-12-06 15:01:04 +0100

    disabled ipu6 driver

--- void-carbonx1.nix
+++ void-carbonx1.nix
@@ -37,11 +37,12 @@
   services.fprintd.enable = true;
 
   # Camera
-  # currently unstable, see https://github.com/NixOS/nixpkgs/issues/225743
-  hardware.ipu6 = {
-    enable = true;
-    platform = "ipu6ep";
-  };
+  # currently unstable, causes system freeze after standby/suspend
+  # see https://github.com/NixOS/nixpkgs/issues/225743
+  #hardware.ipu6 = {
+  #  enable = true;
+  #  platform = "ipu6ep";
+  #};
 
   # FCC Unlock for integrated LTE Modem
   # curently not working, see
