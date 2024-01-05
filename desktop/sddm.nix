a91a3126bc67d0c4cae2ad9db60c1e9be641391c
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2024-01-05 01:45:44 +0100

    wayland support, on screen keyboard workaround

--- desktop/sddm.nix
+++ desktop/sddm.nix
@@ -4,10 +4,35 @@
 
   # Login Manager SDDM with custom Theme 
 
+  # List of interesting themes:
+  # https://store.kde.org/p/1692328
+  # https://store.kde.org/p/2102227
+  # https://store.kde.org/p/2056189
+  # https://store.kde.org/p/2091569
+  # WarGames: https://store.kde.org/p/2072906
+  # https://store.kde.org/p/1994287
+  # Terminal: https://store.kde.org/p/1949572
+  # https://store.kde.org/p/1534637
+  # https://store.kde.org/p/1366843
+  # https://store.kde.org/p/1430814
+  # Zust: https://github.com/stepanzubkov/sddm-zust/tree/main
+  
   services.xserver.displayManager.sddm = {
     enable = true;
     enableHidpi = true;
+    wayland.enable = true;
     theme = "where-is-my-sddm-theme";
+  };
+
+  # Workaround to disable onscreen Keyboard, see: https://github.com/NixOS/nixpkgs/issues/55423
+  #services.xserver.displayManager.sddm.extraConfig = ''
+  #  [General]
+  #  InputMethod=
+  #'';
+  services.xserver.displayManager.sddm.settings = {
+    General = {
+      InputMethod = "";
+    };
   };
 
   environment.systemPackages = let themes = pkgs.callPackage ../nixpkgs/sddm-themes.nix {}; in [ 
