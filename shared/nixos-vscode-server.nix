457c8b707db9e584d81e222bfac909f4c80b9e80
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2024-02-08 10:38:15 +0100

    additional options

--- shared/nixos-vscode-server.nix
+++ shared/nixos-vscode-server.nix
@@ -9,4 +9,15 @@
   ];
 
   services.vscode-server.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vscode
+    nodejs-16_x # Need this for https://nixos.wiki/wiki/Vscode server
+  ];
+
+  # https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_watches" = "524288";
+  };
+
 }
