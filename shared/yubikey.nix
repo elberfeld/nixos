ca23800a1f226c50bd63517fc707234bdfd175bd
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2024-05-11 19:11:28 +0200

    fix yubikey ssh

--- shared/yubikey.nix
+++ shared/yubikey.nix
@@ -2,46 +2,13 @@
 
 {
 
-  # Verwendung des SSH-Schlüssels vom Yubikey
-  #services.yubikey-agent.enable = true;
-
-  #environment.systemPackages = with pkgs; [
-  #  pinentry-tty
-  #];
-
-  #programs.gnupg.agent = {
-  #  enable = false;
-  #  pinentryPackage = pkgs.pinentry-tty;
-  #};
-
-  # Alternative Konfiguration mit dem GnuPG-Agent 
-  # https://nixos.wiki/wiki/Yubikey
-
-  # Stand 20.04.2024: Option disable-ccid in $HOME/.gnupg/scdaemon.conf is needed 
-  # Open Bug, see https://github.com/NixOS/nixpkgs/issues/155629 
-
-  hardware.gpgSmartcards.enable = true;
-
-  services.udev.packages = [ pkgs.yubikey-personalization ];
-  services.pcscd.enable = true;
-
-  programs.ssh.startAgent = false;
+  # see https://nixos.wiki/wiki/Yubikey
   
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    pinentryPackage = pkgs.pinentry-tty;
   };
-
-  environment.systemPackages = with pkgs; [
-    gnupg
-    pinentry-all
-    yubikey-personalization
-  ];
-
-  environment.shellInit = ''
-    gpg-connect-agent /bye
-    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-  '';
 
 }
