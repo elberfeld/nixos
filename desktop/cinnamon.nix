150eea284681ed9bc20183f1e4dac96f083d6666
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2025-01-11 23:57:21 +0100

    cinnamon desktop configuration

--- desktop/cinnamon.nix
+++ desktop/cinnamon.nix
@@ -2,8 +2,6 @@
 
 {
 
-  services.xserver.desktopManager.gnome.enable = false;
-
   # Enable the Cinnamon Desktop Environment.
   services.xserver.desktopManager.cinnamon.enable = true;
   
@@ -11,13 +9,16 @@
   xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-xapp ];
 
   # Exclude default Packages 
-  # environment.cinnamon.excludePackages = with pkgs.kdePackages; [
-  # ];
+  environment.cinnamon.excludePackages = with pkgs; [
+    gnome-screenshot
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-
-  # ];
+  environment.systemPackages = with pkgs; [
+    flameshot
+    gnome-software
+    gnome-system-monitor
+  ];
 
 }
