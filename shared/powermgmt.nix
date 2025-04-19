5a448b2961f176431a2af366601706a2b38c9bf7
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2025-04-19 23:09:04 +0200

    power-profiles-daemon

--- shared/powermgmt.nix
+++ shared/powermgmt.nix
@@ -13,31 +13,7 @@
   # Enable Thermald to prevent overheat on inter CPUs
   services.thermald.enable = true;
 
-  # Disable Prower Profiles Daemon (conflicts with TLP) 
-  services.power-profiles-daemon.enable = false;
-
-  # Enable TLP
-  # https://linrunner.de/tlp/
-  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/hardware/tlp.nix
-  services.tlp.enable = true;
-
-  # Enable Auto-CpuFreq
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-    };
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-  };
-
-  # acpi_call makes tlp work for newer thinkpads
-  boot = {
-    kernelModules = [ "acpi_call" ];
-    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
-  };
+  # Enable Power Profiles Daemon 
+  services.power-profiles-daemon.enable = true;
 
 }
