51259f925cc8723be6dadc5df3ec45d401ae5302
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2025-09-10 07:33:26 +0200

    disable usb autosuspend

--- shared/powermgmt.nix
+++ shared/powermgmt.nix
@@ -16,4 +16,9 @@
   # Enable Power Profiles Daemon 
   services.power-profiles-daemon.enable = true;
 
+  # Disable USB Autosuspend to avoid issues with some USB devices
+  # Avoid USB Keyboard input Lag 
+  # https://discourse.nixos.org/t/external-mouse-and-keyboard-sleep-when-they-stay-untouched-for-a-few-seconds/14900/11
+  boot.kernelParams = [ "usbcore.autosuspend=-1" ];
+
 }
