2859c7e96b30f59c0cb1f978c0baef3ad04eae81
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2025-09-23 20:30:35 +0200

    remove unused option

--- hosts/void-carbonx1-hardware.nix
+++ hosts/void-carbonx1-hardware.nix
@@ -33,14 +33,6 @@
 
   boot.kernelParams = [ "resume=/dev/disk/by-uuid/5e751dc0-b5ab-454a-b4f9-decd8a6ca10f" ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wwan0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
