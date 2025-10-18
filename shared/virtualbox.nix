ecd20929a04601b7b55a8e8cee66d334e12fbd27
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2025-10-18 21:52:38 +0200

    fix virtualbox

--- shared/virtualbox.nix
+++ shared/virtualbox.nix
@@ -4,6 +4,13 @@
 
   # Activate virtualbox 
   # User must be in Group vboxusers to use virtualbox
-  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host = {
+    enable = true;
+    enableKvm = true;
+    addNetworkInterface = false;
+  };
+
+  # see https://discourse.nixos.org/t/issue-with-virtualbox-in-24-11/57607/2
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
 
 }
