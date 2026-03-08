cd7544330993a20786564b34543dfaa415db64ac
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2026-03-08 15:40:32 +0100

    enable steam remote play

--- desktop/gaming.nix
+++ desktop/gaming.nix
@@ -4,7 +4,7 @@
 
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
 
