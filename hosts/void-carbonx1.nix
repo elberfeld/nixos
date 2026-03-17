4757346421b0106d7ff60c0a8004fb6f5d447968
Christian Elberfeld <6413499+elberfeld@users.noreply.github.com>
2026-03-17 19:50:13 +0100

    removing stuff after umts modem disabled in bios

--- hosts/void-carbonx1.nix
+++ hosts/void-carbonx1.nix
@@ -22,41 +22,16 @@
   # Allow UDP port for AusweisApp
   networking.firewall.allowedUDPPorts = [ 24727 ];
   
-  # Lenovo Device Info
-  # https://psref.lenovo.com/Detail/ThinkPad_X1_Carbon_Gen_10?M=21CB00AEMB
-  
-  # Arch Wiki with Hardware Infos 
-  # https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_10)
-
+  # Lenovo Device Info: https://psref.lenovo.com/Detail/ThinkPad_X1_Carbon_Gen_10?M=21CB00AEMB
+  # Arch Wiki with Hardware Infos: https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_10)
+  # UMTS Modem deactivated in wiki (no FCC unlock, causes hibernation to hang)
 
   # Fingerprint Sensor 
   # Causes Login delay, see https://github.com/NixOS/nixpkgs/issues/239770
   services.fprintd.enable = true;
 
-  # Blacklist the iosm module (Intel LTE modem driver) because it causes hibernation to hang
-  # with "msg timeout" errors. The modem doesn't work anyway (FCC unlock issue).
-  # boot.blacklistedKernelModules = [ "iosm" ];
-  # Note: blacklisting the module breaks the fingeprint Reader
-  # => Deactivated Wireless WAN in BIOS settings 
-
-  # exclude WWAN from Powersave 
-  # see https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_10)
-  #services.tlp.settings = {
-  #  RUNTIME_PM_DENYLIST="08:00.0";
-  #};
-
-  #fonts.fontconfig.dpi = 135;
-  #i18n.consoleFont = "ter-132b";
-  #i18n.consolePackages = with pkgs; [ terminus_font ];
-  #boot.earlyVconsoleSetup = true;  
-
-  # set up console font for boot 
-  #console = {
-  #  earlySetup = true;
-  #  font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
-  #  packages = with pkgs; [ terminus_font ];
-  #};
-
+  # Disable fingerprint authentication for console login as fallback 
+  security.pam.services.login.fprintAuth = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
